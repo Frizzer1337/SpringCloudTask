6ffd982fd46b053856525526b9ee083d1a3e3a08
Frizzer1337 <81990525+Frizzer1337@users.noreply.github.com>
2023-03-14 16:01:54 +0300

    Changes in namings of PaymentApiApplication class

--- payment-api/src/main/kotlin/com/frizzer/kafka/paymentapi/PaymentApiApplicaton.kt
+++ payment-api/src/main/kotlin/com/frizzer/kafka/paymentapi/PaymentApiApplicaton.kt
@@ -6,8 +6,8 @@ import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
-open class PaymentApiApplicaton
+open class PaymentApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<PaymentApiApplicaton>(*args)
+    runApplication<PaymentApiApplication>(*args)
 }
